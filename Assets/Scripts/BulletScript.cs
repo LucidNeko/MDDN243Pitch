ace7d90a4d1bc4a5cc81119967108075428b7d13
Hamish <LucidNeko@users.noreply.github.com>
2015-08-13 11:54:43 +1200

    Initial Commit

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	public void Awake() {
+		StartCoroutine(Expand (6, 1, 0.001f));
+	}	
+
+	public void OnCollisionEnter(Collision collision) {
+//		Destroy (gameObject);
+		MakePaint (collision);
+	}
+
+	private void MakePaint(Collision info) {
+
+	}
+
+	public IEnumerator Expand(float size, float duration, float delay) {
+//		yield return new WaitForSeconds (delay);
+
+		Vector3 start = transform.localScale;
+		Vector3 end = new Vector3 (size, size, size);
+
+		float t = 0;
+		while (t < 1) {
+			transform.localScale = Vector3.Lerp(start, end, t);
+			t += Time.deltaTime * duration;
+			yield return null;
+		}
+	}
+
+}
---
+++ Assets/Scripts/FPSCameraScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSCameraScript : MonoBehaviour {
+
+	public Transform m_Target;
+	public bool m_InvertX = false;
+	public bool m_InvertY = true;
+	public float m_SpeedX = 6f;
+	public float m_SpeedY = 6f;
+
+	private GameObject m_Player;
+	private Rigidbody m_PlayerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		GameObject m_Player = GameObject.FindGameObjectWithTag ("Player");
+		m_PlayerRigidBody = m_Player.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		forward.Scale (new Vector3(1, 0, 1));
+		Quaternion look = Quaternion.LookRotation (forward);
+
+//		m_PlayerRigidBody.MoveRotation (look);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = m_Target.position;
+
+		float dx = Input.GetAxis ("Mouse X") * m_SpeedX;
+		float dy = Input.GetAxis ("Mouse Y") * m_SpeedY;
+		if(m_InvertX) { dx = -dx; }
+		if(m_InvertY) { dy = -dy; }
+
+		transform.Rotate (0, dx, 0, Space.World);
+		transform.Rotate (dy, 0, 0, Space.Self);
+	}
+}
