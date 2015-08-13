fc6da37df119d9949f583b41e3af8d4025a66dca
Guest User <Guest@f-03-06-lab.mddn.vuw.ac.nz>
2015-08-13 21:58:15 +1200

    Add paint splat. No Light on paint though

--- Assets/PaintScript.cs
+++ Assets/PaintScript.cs
@@ -4,22 +4,42 @@ using System.Collections;
 public class PaintScript : MonoBehaviour {
 
 	public Texture2D m_PaintMask;
+	public int m_TextureResolution = 1024;
+
 
 	private Texture2D m_Texture;
+	private RenderTexture m_RenderTexture;
 	private Color32[] m_PaintMaskPixels;
 
 	// Use this for initialization
 	void Start () {
-		m_PaintMaskPixels = m_PaintMask.GetPixels32 ();
+//		m_Texture = GetComponent<Renderer> ().material.mainTexture as Texture2D;
 
-		m_Texture = GetComponent<Renderer> ().material.mainTexture as Texture2D;
-		Texture2D temp = new Texture2D(m_Texture.width, m_Texture.height, m_Texture.format, m_Texture.mipmapCount != 0);
+//		m_RenderTexture = new RenderTexture (m_Texture.width, m_Texture.height, 24, RenderTextureFormat.ARGB32);
+//		Graphics.Blit (m_PaintMask, m_RenderTexture);
+//		GetComponent<Renderer> ().material.mainTexture = m_RenderTexture;
 
-		var pixels = m_Texture.GetPixels32 ();
-		temp.SetPixels32(pixels);
-		temp.Apply ();
 
-		m_Texture = temp;
+
+//		m_PaintMaskPixels = m_PaintMask.GetPixels32 ();
+//
+//		m_Texture = GetComponent<Renderer> ().material.mainTexture as Texture2D;
+
+//		Texture2D temp = new Texture2D(m_Texture.width, m_Texture.height, m_Texture.format, m_Texture.mipmapCount != 0);
+//
+//		var pixels = m_Texture.GetPixels32 ();
+//		temp.SetPixels32(pixels);
+//		temp.Apply ();
+//
+//		m_Texture = temp;
+		Color[] white = new Color[m_TextureResolution * m_TextureResolution];
+		for(int i = 0; i < m_TextureResolution * m_TextureResolution; i++) {
+			white[i] = Color.white;
+		}
+
+		m_Texture = new Texture2D (m_TextureResolution, m_TextureResolution, TextureFormat.RGBA32, false);
+		m_Texture.SetPixels (white);
+		m_Texture.Apply ();
 		GetComponent<Renderer> ().material.mainTexture = m_Texture;
 	}
 	
@@ -29,8 +49,34 @@ public class PaintScript : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision info) {
-		m_Texture.SetPixels32 (m_PaintMaskPixels);
-		m_Texture.Apply ();
+//		m_Texture.SetPixels32 (m_PaintMaskPixels);
+//		m_Texture.Apply ();
+
+		foreach (ContactPoint p in info.contacts) {
+
+			RaycastHit hit = new RaycastHit();
+			Ray ray = new Ray(p.point - p.normal, p.normal);
+			if(p.thisCollider.Raycast(ray, out hit, 1.1f)){
+				Vector2 texCoord = hit.textureCoord;
+				Debug.Log(texCoord);
+				int x = (int)(texCoord.x * m_Texture.width);
+				int y = (int)(texCoord.y * m_Texture.height);
+
+				int width = 51;
+				int height = 51;
+				int x0 = x - width/2;
+				int y0 = y - height/2;
+
+				Color[] colors = new Color[width*height];
+				for(int i = 0; i < width*height; i++) {
+					colors[i] = Color.red;
+				}
+
+				m_Texture.SetPixels(x0, y0, width, height, colors);
+//				m_Texture.SetPixel(x, y, Color.red);
+				m_Texture.Apply();
+			}
+		}
 
 
 //		for (int i = 0; i < 100; i++) {
